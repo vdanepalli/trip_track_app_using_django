063590d6178d4c6b2bb2cfedebcb629d07436c5e
Vinay Danepalli <vdanepalli@gmail.com>
2024-04-07 18:05:32 -0400

    unset debug and allowed all hosts

--- config/settings.py
+++ config/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p@7lladhh42#c#$s$+&rf5na1g@25h=l3205!g923pct%i+kd^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
